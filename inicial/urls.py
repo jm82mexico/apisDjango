59b38c45962f2b303fee3758d29343ee3a6b4296
jm82mexico <jmdelan2012@gmail.com>
2022-10-28 21:34:12 -0500

    Crear documentación

--- inicial/urls.py
+++ inicial/urls.py
@@ -16,11 +16,27 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path,include
-# from envios.views import Helloword
 from envios.api.router import router_post
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="POST API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="jmdelan2012@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('hola/',Helloword.as_view()),
-    path('api/',include(router_post.urls)),
+    path('admin/', admin.site.urls),    
+    path('api/',include(router_post.urls)),    
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
