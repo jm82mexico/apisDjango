120a49205d3cd9a1928ce44599e06d3a9917c0d8
jm82mexico <jmdelan2012@gmail.com>
2022-10-28 21:11:36 -0500

    Agregar permisos del framework

--- envios/api/views.py
+++ envios/api/views.py
@@ -5,7 +5,9 @@ from rest_framework.viewsets import ViewSet,ModelViewSet
 from rest_framework.response import Response
 from envios.models import Post
 from envios.api.serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
 
 class PostModelViewSet(ModelViewSet):
+   permission_classes=[IsAuthenticatedOrReadOnly]
    serializer_class = PostSerializer
    queryset = Post.objects.all()
