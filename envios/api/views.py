2a800a7e4a92726c85ae8ba1f88ee5dd3a9ac15d
jm82mexico <jmdelan2012@gmail.com>
2022-10-28 21:16:19 -0500

    Permisos personalizados

--- envios/api/views.py
+++ envios/api/views.py
@@ -6,8 +6,9 @@ from rest_framework.response import Response
 from envios.models import Post
 from envios.api.serializers import PostSerializer
 from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
+from envios.api.permissions import isAdminOrReadOnly
 
 class PostModelViewSet(ModelViewSet):
-   permission_classes=[IsAuthenticatedOrReadOnly]
+   permission_classes=[isAdminOrReadOnly]
    serializer_class = PostSerializer
    queryset = Post.objects.all()
