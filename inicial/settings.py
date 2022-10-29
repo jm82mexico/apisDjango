dd6adf3f22559187982defbf1a6a88c7008e9f84
jm82mexico <jmdelan2012@gmail.com>
2022-10-29 13:59:41 -0500

    Modificar duración tokens

--- inicial/settings.py
+++ inicial/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -109,6 +109,11 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
     
+}
+
+SIMPLE_JWT ={
+    'ACCES_TOKEN_LIFETIME' : datetime.timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME' : datetime.timedelta(days=7),
 }
 
 
