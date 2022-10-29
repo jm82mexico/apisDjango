6dc68c84a74e2343d44c6220bd21bffd2ee2d4fc
jm82mexico <jmdelan2012@gmail.com>
2022-10-28 20:57:46 -0500

    Configurar PostViewModel

--- envios/api/router.py
+++ envios/api/router.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
-from envios.api.views import PostViewSet
+from envios.api.views import PostModelViewSet
 
 router_post = DefaultRouter()
 
-router_post.register(prefix='posts',basename='posts',viewset=PostViewSet)
+router_post.register(prefix='posts',basename='posts',viewset=PostModelViewSet)
--- envios/api/views.py
+++ envios/api/views.py
@@ -1,23 +1,11 @@
 from urllib import response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ViewSet,ModelViewSet
 from rest_framework.response import Response
 from envios.models import Post
 from envios.api.serializers import PostSerializer
 
-class PostViewSet(ViewSet):
-    def list(self,request):
-        serializer = PostSerializer(Post.objects.all(),many=True)        
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
-    
-    def retrieve(self,request, pk: int):
-        post = PostSerializer(Post.objects.get(pk=pk))
-        print(post.data)
-        return Response(status=status.HTTP_200_OK, data=post.data)
-
-    def create(self,request):
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status=status.HTTP_200_OK,data=serializer.data)
+class PostModelViewSet(ModelViewSet):
+   serializer_class = PostSerializer
+   queryset = Post.objects.all()
